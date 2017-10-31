bedb508b80dafcd17491d9cb8800bba345abce68
Mark Han <markwhan11@gmail.com>
2017-10-30 21:06:46 -0400

    use while loop to create textviews for words ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,8 +1,9 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -25,17 +26,15 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        Log.v("NumbersActivity", "Word at Index 0: " + words.get(0));
-        Log.v("NumbersActivity", "Word at Index 1: " + words.get(1));
-        Log.v("NumbersActivity", "Word at Index 2: " + words.get(2));
-        Log.v("NumbersActivity", "Word at Index 3: " + words.get(3));
-        Log.v("NumbersActivity", "Word at Index 4: " + words.get(4));
-        Log.v("NumbersActivity", "Word at Index 5: " + words.get(5));
-        Log.v("NumbersActivity", "Word at Index 6: " + words.get(6));
-        Log.v("NumbersActivity", "Word at Index 7: " + words.get(7));
-        Log.v("NumbersActivity", "Word at Index 8: " + words.get(8));
-        Log.v("NumbersActivity", "Word at Index 9: " + words.get(9));
+        int index = 0;
 
+        while (index < words.size()) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
+        }
     }
 }
