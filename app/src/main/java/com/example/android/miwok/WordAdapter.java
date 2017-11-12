3fae7232a1e08216052be1b99850ec6336142e50
Mark Han <markwhan11@gmail.com>
2017-11-12 15:05:36 -0500

    create WordAdapter custom class which allows 2 textviews

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,55 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by markhan on 11/12/17.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word>{
+
+    public WordAdapter(Context context, ArrayList words) {
+        super(context,0, words);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.item_list, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the default word from the current Word object and
+        // set this text on the name default_text_view TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the miwok translation from the current Word object and
+        // set this text on the miwok_text_view TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+
+        return listItemView;
+
+
+    }
+}
