e00200e46c59b1bfd3c85a0bad4abb355b195bac
unknown <markwhan11@gmail.com>
2018-01-29 01:21:54 -0500

    add capability of WordAdapter to match iconImage with current word

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,6 +23,8 @@ public class WordAdapter extends ArrayAdapter<Word>{
     }
 
 
+
+    // Purpose of getView method is to return listitem view and return it to the listView
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -35,18 +38,23 @@ public class WordAdapter extends ArrayAdapter<Word>{
         // Get the {@link Word} object located at this position in the list
         Word currentWord = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the ID version_name
+        // Find the TextView in the list_item.xml layout
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         // Get the default word from the current Word object and
         // set this text on the name default_text_view TextView
         defaultTextView.setText(currentWord.getDefaultTranslation());
 
-        // Find the TextView in the list_item.xml layout with the ID version_number
+        // Find the TextView in the list_item.xml layout
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         // Get the miwok translation from the current Word object and
         // set this text on the miwok_text_view TextView
         miwokTextView.setText(currentWord.getMiwokTranslation());
 
+        // Find the ImageView in the list_item.xml layout
+        ImageView iconImage = (ImageView) listItemView.findViewById(R.id.iconImage);
+        // Get the appropriate image from the current object and
+        // set this resource id on the imageView
+        iconImage.setImageResource(currentWord.getImageResourceId());
 
         return listItemView;
 
