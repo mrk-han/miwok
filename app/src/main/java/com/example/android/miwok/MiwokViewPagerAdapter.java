3619c19df52cec6d4da476544e8c79398a24353c
Mark Han <markwhan11@gmail.com>
2018-03-07 00:04:03 -0500

    add documentation

--- app/src/main/java/com/example/android/miwok/MiwokViewPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/MiwokViewPagerAdapter.java
@@ -8,13 +8,26 @@ import android.support.v4.app.FragmentStatePagerAdapter;
  * Created by markhan on 3/6/18.
  */
 
+/**
+ * {@link MiwokViewPagerAdapter} is a {@link MiwokViewPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
 public class MiwokViewPagerAdapter extends FragmentStatePagerAdapter {
 
 
+    /**
+     * Create a new {@link MiwokViewPagerAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
     public MiwokViewPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
     @Override
     public Fragment getItem(int position) {
         if (position == 0) {
@@ -28,6 +41,9 @@ public class MiwokViewPagerAdapter extends FragmentStatePagerAdapter {
         }
     }
 
+    /**
+     * Return the total number of pages.
+     */
     @Override
     public int getCount() {
         return 4;
